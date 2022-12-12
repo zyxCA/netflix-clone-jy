6af51cdc2854adcc6ad0fc7cdffdc7db1bb31240
jin <jin>
2022-12-12 18:39:09 -0500

    first

---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+// import {logo} from "../asset/img/logo.svg"
+import { HiSearch } from "react-icons/hi"
+import Image from "next/image"
+import { BellIcon } from "@heroicons/react/24/solid"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+function Header() {
+  const [scroll, setScroll] = useState(false)
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      setScroll(true)
+    } else {
+      setScroll(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+  return (
+    <header className={`${scroll && "bg-slate-500"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className=" cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 font-light">
+        <HiSearch className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
